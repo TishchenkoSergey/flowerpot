f4aff07107ddae294c8878b4fdef058ae217f682
Serhii <tishchenko.seriy@gmail.com>
2024-11-14 16:39:17 +0200

    Init project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlowerpotApp());
+}
+
+class FlowerpotApp extends StatelessWidget {
+  const FlowerpotApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}

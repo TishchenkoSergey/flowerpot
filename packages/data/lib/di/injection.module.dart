4d97b447cc6366e719a5d8d54ed9102744e10614
Serhii <tishchenko.seriy@gmail.com>
2024-12-08 13:48:42 +0200

    feat: inject SharedPreferences to data

--- packages/data/lib/di/injection.module.dart
+++ packages/data/lib/di/injection.module.dart
@@ -6,6 +6,7 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'dart:async' as _i687;
 
+import 'package:data/di/injection.dart' as _i237;
 import 'package:data/repository/game_parameters_repository_impl.dart' as _i411;
 import 'package:data/repository/game_session_repository_impl.dart' as _i798;
 import 'package:data/repository/game_start_time_repository_impl.dart' as _i112;
@@ -17,7 +18,12 @@ import 'package:shared_preferences/shared_preferences.dart' as _i460;
 class DataPackageModule extends _i526.MicroPackageModule {
 // initializes the registration of main-scope dependencies inside of GetIt
   @override
-  _i687.FutureOr<void> init(_i526.GetItHelper gh) {
+  _i687.FutureOr<void> init(_i526.GetItHelper gh) async {
+    final registerModule = _$RegisterModule();
+    await gh.factoryAsync<_i460.SharedPreferences>(
+      () => registerModule.prefs(),
+      preResolve: true,
+    );
     gh.factory<_i494.GameParametersRepository>(() =>
         _i411.GameParametersRepositoryImpl(gh<_i460.SharedPreferences>()));
     gh.factory<_i494.GameStartTimeRepository>(
@@ -32,3 +38,5 @@ class DataPackageModule extends _i526.MicroPackageModule {
         ));
   }
 }
+
+class _$RegisterModule extends _i237.RegisterModule {}

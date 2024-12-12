74d5532206dbb0e9567944534c1f89a689616122
Serhii <tishchenko.seriy@gmail.com>
2024-12-12 17:56:51 +0200

    feat: add base info dialog widget

---
+++ lib/features/game_play_screen/widgets/info_dialog.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class InfoDialog extends StatelessWidget {
+  const InfoDialog({
+    required this.title,
+    this.description,
+    this.positiveTitleButton,
+    this.negativeTitleButton,
+    this.onPressedPositive,
+    this.onPressedNegative,
+    super.key,
+  });
+
+  final String title;
+  final String? description;
+  final String? positiveTitleButton;
+  final String? negativeTitleButton;
+  final VoidCallback? onPressedPositive;
+  final VoidCallback? onPressedNegative;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              description!,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            IntrinsicWidth(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: onPressedNegative,
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
+                    child: Text(negativeTitleButton!),
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: onPressedPositive,
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
+                    child: Text(positiveTitleButton!),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
